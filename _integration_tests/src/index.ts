c492a7d269b4ee3ddd075f68a3ecea945413dd01
ElRaptorus <christian.werner@5minds.de>
2018-12-14 14:28:56 +0100

    ⬆️ Upgrade test setup
    **Changes:**
    
    - Upgrade ProcessEngine and ProcessEngineContracts
    - Globally use Bluebird as the default Promise library
    - Reduce some timeouts used by the `TimerBoundaryEvent`, `TimerStartEvent` und `IntermediateTimerCatchEvent` tests.
    - Fix some typos in the process diagrams.
    - Reduce the polling interval used by the `ProcessInstanceHandler` when waiting for a suspended task.

--- _integration_tests/src/index.ts
+++ _integration_tests/src/index.ts
@@ -1,2 +1,10 @@
+import * as Bluebird from 'bluebird';
+
+Bluebird.config({
+  cancellation: true,
+});
+
+global.Promise = Bluebird;
+
 export * from './fixture_providers/index';
 export * from './mocks/index';
