c119b5afa609515aad8bbf3b5c5cd10d5cd2f1ae
Vyperus <bastian.schnittger@5minds.de>
2019-03-06 14:26:51 +0100

    :recycle: Add Requested Changes

--- _integration_tests/test/process_models/terminate_process_instance.js
+++ _integration_tests/test/process_models/terminate_process_instance.js
@@ -4,9 +4,9 @@ const should = require('should');
 
 const StartCallbackType = require('@process-engine/management_api_contracts').DataModels.ProcessModels.StartCallbackType;
 
-const {TestFixtureProvider, ProcessInstanceHandler} = require('../../dist/commonjs');
+const {ProcessInstanceHandler, TestFixtureProvider} = require('../../dist/commonjs');
 
-const testCase = 'Management API:   POST  ->  /process_models/:process_model_id/start?start_callback_type=1&start_event_id=:start_event_id';
+const testCase = 'Management API:   POST  ->  /process_instance/:process_instance_id/terminate';
 
 describe(`Management API: ${testCase}`, () => {
 
@@ -29,7 +29,6 @@ describe(`Management API: ${testCase}`, () => {
     ];
 
     await testFixtureProvider.importProcessFiles(processModelsToImport);
-
   });
 
   after(async () => {
