06d70b90736db5a237177c20ab03b1a4cec71eec
Steffen <Steffen.knaup@5minds.de>
2019-09-17 13:18:44 +0200

    :pencil2: Fix Typo

--- _integration_tests/test/flow_node_instances/get_waiting_for_correlation.js
+++ _integration_tests/test/flow_node_instances/get_waiting_for_correlation.js
@@ -5,7 +5,7 @@ const uuid = require('node-uuid');
 
 const {TestFixtureProvider, ProcessInstanceHandler} = require('../../dist/commonjs');
 
-describe('Management API: getSuspendedTasksForCorrelation', () => {
+describe('Management API: GetSuspendedTasksForCorrelation', () => {
 
   let eventAggregator;
   let processInstanceHandler;
