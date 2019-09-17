88a9817fdd8225ca39a11d9ac16da0f747e609af
Steffen <Steffen.knaup@5minds.de>
2019-09-17 14:18:11 +0200

    :pencil2: Fix Typo

--- _integration_tests/test/flow_node_instances/get_all_suspended.js
+++ _integration_tests/test/flow_node_instances/get_all_suspended.js
@@ -5,7 +5,7 @@ const uuid = require('node-uuid');
 
 const {TestFixtureProvider, ProcessInstanceHandler} = require('../../dist/commonjs');
 
-describe('Management API: getAllSuspendedTasks', () => {
+describe('Management API: GetAllSuspendedTasks', () => {
 
   let processInstanceHandler;
   let testFixtureProvider;
